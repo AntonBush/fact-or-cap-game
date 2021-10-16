ec9ac19b46e38c2c7aa6dbf4254206d21f56cd26
AntonBush <bushev.anton.ru@yandex.ru>
2021-10-16 21:15:47 +0300

    Сформированы интерфейсы взаимодействия с API

---
+++ app/src/main/java/com/tmvlg/factorcapgame/data/repository/fact/FactApi.kt
@@ -0,0 +1,18 @@
+package com.tmvlg.factorcapgame.data.repository.fact
+
+import com.tmvlg.factorcapgame.data.repository.fact.asli.AsliFactApiService
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
+
+open class FactApi<T : FactApiService>(
+    baseUrl: String,
+    apiService: Class<T>
+) {
+    val retrofitService: FactApiService by lazy {
+        Retrofit.Builder()
+            .addConverterFactory(JacksonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .build()
+            .create(apiService)
+    }
+}
---
+++ app/src/main/java/com/tmvlg/factorcapgame/data/repository/fact/FactApiService.kt
@@ -0,0 +1,8 @@
+package com.tmvlg.factorcapgame.data.repository.fact
+
+import retrofit2.http.GET
+
+interface FactApiService {
+    @GET
+    suspend fun getFact(): Fact
+}
