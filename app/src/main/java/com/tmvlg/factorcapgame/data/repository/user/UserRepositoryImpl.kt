c4d699b65e023fe8935adf7e174a52f4e35d3ef6
Mikheikin Dmitry <2madeira@mail.ru>
2021-10-21 21:38:20 +0300

    added empty class model user)

--- app/src/main/java/com/tmvlg/factorcapgame/data/repository/user/UserRepositoryImpl.kt
+++ app/src/main/java/com/tmvlg/factorcapgame/data/repository/user/UserRepositoryImpl.kt
@@ -1,4 +1,9 @@
 package com.tmvlg.factorcapgame.data.repository.user
 
-class UserRepositoryImpl: UserRepository {
+class UserRepositoryImpl(val username : String,
+                         val total_games : Int,
+                         val highest_score : Int,
+                         val last_score : Int,
+                         val average_score : Int,
+                         val all_scores : Int): UserRepository{
 }
