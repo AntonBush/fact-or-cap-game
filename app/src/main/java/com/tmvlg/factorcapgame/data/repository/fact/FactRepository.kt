288b9ef3681c6d1e0d0782c79469a74023e59e5b
AntonBush <bushev.anton.ru@yandex.ru>
2021-10-24 21:09:06 +0300

    Получение фактов только на рабочем потоке

--- app/src/main/java/com/tmvlg/factorcapgame/data/repository/fact/FactRepository.kt
+++ app/src/main/java/com/tmvlg/factorcapgame/data/repository/fact/FactRepository.kt
@@ -1,11 +1,13 @@
 package com.tmvlg.factorcapgame.data.repository.fact
 
+import androidx.annotation.WorkerThread
 import com.tmvlg.factorcapgame.data.repository.fact.asli.AsliFactApi
 import com.tmvlg.factorcapgame.data.repository.fact.genfun.GenfunFactApi
 
 class FactRepository(
     private val factApiList: List<FactApi<*>>
 ) {
+    @WorkerThread
     suspend fun getFact(): Fact {
         return factApiList.random().retrofitService.getFact()
     }
