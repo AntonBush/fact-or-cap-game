5206c57938588859f7f20d5e62148824658a5730
Galansev Egor <galancc@mail.ru>
2021-10-24 20:37:07 +0300

    Добавил базовую навигацию с переносом имени пользователя между существующими страницами, добавил заглушки на несуществующие страницы, макеты под проверки имени пользователя, исправил xml меню и статистики

--- app/src/main/java/com/tmvlg/factorcapgame/ui/MainActivity.kt
+++ app/src/main/java/com/tmvlg/factorcapgame/ui/MainActivity.kt
@@ -2,12 +2,32 @@ package com.tmvlg.factorcapgame.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat.setAlpha
+import androidx.fragment.app.Fragment
 import com.tmvlg.factorcapgame.R
+import com.tmvlg.factorcapgame.ui.menu.CreateUsernameFragment
+import com.tmvlg.factorcapgame.ui.menu.MenuFragment
 import com.tmvlg.factorcapgame.ui.singlegame.SingleGameFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment : Fragment
+        if (hasValidUsername())
+            fragment = MenuFragment()
+        else
+            fragment = CreateUsernameFragment()
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_fragment_container,fragment)
+            .commit()
     }
+
+    private fun hasValidUsername() : Boolean {
+        // Здесь смотреть не зарегистрировался ли уже пользователь
+        return false
+    }
+
 }
--- app/src/main/java/com/tmvlg/factorcapgame/ui/statisitics/StatisticsFragment.kt
+++ app/src/main/java/com/tmvlg/factorcapgame/ui/statisitics/StatisticsFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.tmvlg.factorcapgame.R
 import com.tmvlg.factorcapgame.databinding.FragmentStatisticsBinding
+import com.tmvlg.factorcapgame.ui.menu.MenuFragment
 
 class StatisticsFragment : Fragment() {
 
@@ -23,6 +26,12 @@ class StatisticsFragment : Fragment() {
     ): View {
 
         _binding = FragmentStatisticsBinding.inflate(inflater, container, false)
+
+        binding.returnButton.setOnClickListener(){
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.main_fragment_container,MenuFragment())
+                .commit()
+        }
 
         return binding.root
     }
