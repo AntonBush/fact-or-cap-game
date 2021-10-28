da9631a95275d2ea33884436d220ea49e0f4f3da
Galansev Egor <galancc@mail.ru>
2021-10-28 21:06:46 +0300

    Исправил для автотеста

--- app/src/main/java/com/tmvlg/factorcapgame/ui/menu/CreateUsernameFragment.kt
+++ app/src/main/java/com/tmvlg/factorcapgame/ui/menu/CreateUsernameFragment.kt
@@ -1,6 +1,5 @@
 package com.tmvlg.factorcapgame.ui.menu
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,12 +7,10 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import android.widget.TextView
 import com.tmvlg.factorcapgame.R
 import com.tmvlg.factorcapgame.databinding.FragmentCreateUsernameBinding
-import android.view.KeyEvent
 
-import android.widget.EditText
-import android.widget.TextView
 import com.tmvlg.factorcapgame.ui.singlegame.SingleGameFragment
 
 
