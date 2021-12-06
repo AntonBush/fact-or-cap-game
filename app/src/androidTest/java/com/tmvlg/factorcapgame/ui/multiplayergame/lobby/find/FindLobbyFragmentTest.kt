e37e52131c186d96b24e8baafdb56080c1910499
Mikheikin Dmitry <2madeira@mail.ru>
2021-12-06 23:05:11 +0300

    Added FindLobbyFragmentTest

--- app/src/androidTest/java/com/tmvlg/factorcapgame/ui/multiplayergame/lobby/find/FindLobbyFragmentTest.kt
+++ app/src/androidTest/java/com/tmvlg/factorcapgame/ui/multiplayergame/lobby/find/FindLobbyFragmentTest.kt
@@ -1,4 +1,4 @@
-package com.tmvlg.factorcapgame.ui.multiplayergame
+package com.tmvlg.factorcapgame.ui.multiplayergame.lobby.find
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -6,14 +6,15 @@ import com.tmvlg.factorcapgame.BaseTest
 import com.tmvlg.factorcapgame.R
 import com.tmvlg.factorcapgame.ui.MainActivity
 import com.tmvlg.factorcapgame.ui.menu.MenuFragmentTest.Companion.menuElems
-import com.tmvlg.factorcapgame.ui.statisitics.StatisticsFragment
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
+
 @RunWith(AndroidJUnit4::class)
-class MultiplayerGameFinishedTest : BaseTest(){
+class FindLobbyFragmentTest : BaseTest(){
 
 
     @get:Rule
@@ -25,38 +26,36 @@ class MultiplayerGameFinishedTest : BaseTest(){
     fun setup() {
         activityRule.scenario.onActivity {
             it.supportFragmentManager.beginTransaction()
-                .replace(R.id.main_fragment_container, MultiplayerGameFinished.newInstance(13,"2313"))
+                .replace(R.id.main_fragment_container, FindLobbyFragment())
                 .commit()
         }
 
     }
 
     @Test
-    fun elementsFinishedMiltiplayerGameIsDisplayed(){
-        checkDisplayedAll(*gameFinishedElems)
+    fun elementsFindLobbyIsDisplayed(){
+        checkDisplayedAll(*findLobbyElems)
     }
 
     @Test
     fun goToMenu(){
-        Thread.sleep(10000)
-        click(R.id.menu_button)
+        click(R.id.return_button)
         checkDisplayedAll(*menuElems)
     }
 
-
     @Test
-    fun goToLobby(){
-        click(R.id.lobby)
+    fun inviteToRoom(){
 
     }
 
 
-
     companion object {
-        val gameFinishedElems = intArrayOf(
-            R.id.tv_game_result,
-            R.id.find_lobby_button,
-            R.id.menu_button
+        val findLobbyElems = intArrayOf(
+            R.id.tv_join_lobby,
+            R.id.find_lobby_edittext,
+            R.id.menu_button,
+            R.id.join_button,
+            R.id.return_button
         )
-   }
+    }
 }
